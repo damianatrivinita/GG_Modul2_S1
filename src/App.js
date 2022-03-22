9c2783076a8c847ed833bd6a17bd2231d314c229
19102242 <19102242@ittelkom-pwt.ac.id>
2022-03-23 00:32:26 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './ex_data';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <img className="album-img" src={data.album.images[0].url} width="180" height="180" />
+      </div>
+      <div>
+        <h2>{data.name}</h2>
+        <h3>{data.album.name}</h3>
+        <p>{data.artists[0].name}</p>
+	<button className="btn"><a href={data.album.artists[0].external_urls.spotify}>Select</a></button>
+      </div>
     </div>
   );
 }
